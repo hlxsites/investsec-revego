985790b751ddbb52fc5251c495e925e7f76018f6
Felix Delval <fdelval@adobe.com>
2023-07-14 16:22:11 +0200

    Starting work on the footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,37 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { readBlockConfig, decorateIcons, decorateSections, createOptimizedPicture, toClassName} from '../../scripts/lib-franklin.js';
+
+/**
+ * Wraps images followed by links within a matching <a> tag.
+ * @param {Element} container The container element
+ */
+function wrapImgsInLinks(container) {
+  const pictures = container.querySelectorAll('picture');
+  pictures.forEach((pic) => {
+    const link = pic.nextElementSibling;
+    if (link && link.tagName === 'A' && link.href) {
+      link.innerHTML = pic.outerHTML;
+      pic.replaceWith(link);
+    }
+  });
+}
+
+function decorateSectionMetadata(section) {
+  /* process section metadata */
+  const sectionMeta = section.querySelector('div.section-metadata');
+  if (sectionMeta) {
+    const meta = readBlockConfig(sectionMeta);
+    Object.keys(meta).forEach((key) => {
+      if (key === 'style') {
+        const styles = meta.style.split(',').map((style) => toClassName(style.trim()));
+        styles.forEach((style) => section.classList.add(style));
+        section.classList.add('section');
+      } else {
+        section.dataset[toCamelCase(key)] = meta[key];
+      }
+    });
+    sectionMeta.remove();
+  }
+}
 
 /**
  * loads and decorates the footer
@@ -20,6 +53,17 @@ export default async function decorate(block) {
     footer.innerHTML = html;
 
     decorateIcons(footer);
+
+    Array.from(footer.children).forEach((section) => {
+      decorateSectionMetadata(section);
+    });
+    footer.querySelector('.edges').querySelectorAll('img').forEach(
+      (img) => 
+        img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '178' }]))
+    );
+
     block.append(footer);
+
+    wrapImgsInLinks(block);
   }
 }
