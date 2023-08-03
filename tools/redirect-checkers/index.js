6a73871167ec16296ca4049145e3b8f91f3a9fdd
Felix Delval <fdelval@adobe.com>
2023-08-03 12:08:02 +0200

    Working redirect checkers

--- tools/redirect-checkers/index.js
+++ tools/redirect-checkers/index.js
@@ -4,6 +4,9 @@ const franklin = process.argv[3];
 import Sitemapper from 'sitemapper';
 import fetch from 'node-fetch';
 
+const success = [];
+const failures = [];
+
 (async () => {
   const site = new Sitemapper({
     url,
@@ -12,13 +15,24 @@ import fetch from 'node-fetch';
 
   try {
     const { sites } = await site.fetch();
-    sites.forEach((url) => {
+
+    for (const url of sites) {
       const parsedUrl = new URL(url);
-      const newUrl = franklin + parsedUrl.pathname;
-      const response = fetch(newUrl, {}).then((response) => {
-        console.log(response.status, newUrl);
+      const newUrl = new URL (franklin + parsedUrl.pathname);
+      await fetch(newUrl, {}).then((response) => {
+        if (response.status >= 299) {
+          failures.push({url: newUrl.toString(), status: response.status});
+        } else {
+          success.push({url: newUrl.toString(), status: response.status});
+        }
       });
-    });
+    }
+
+    console.log('Successes:');
+    console.log(success);
+    console.log('Failures:');
+    console.log(failures);
+
   } catch (error) {
     console.log(error);
     process.exit(1);
