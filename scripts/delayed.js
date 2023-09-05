f0c23343cd0e0dfb8d64519ca9dcfc088ab00cf4
Felix Delval <fdelval@adobe.com>
2023-09-05 14:46:08 +0200

    Fixing linting

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -7,20 +7,20 @@ sampleRUM('cwv');
 // add more delayed functionality here
 
 // Adobe Analytics tracking script
-var dataLayer = {};
-var pageDir = window.location.pathname.replace(/\//g, '');
+let dataLayer = {};
+const pageDir = window.location.pathname.replace(/\//g, '');
 
-if (window.location.pathname != '/') {
- 	dataLayer = {
-		"page": {
-			"articleTitle": pageDir,
-			"name": 'fund-managers/news/' +  pageDir
-		}
-	}
+if (window.location.pathname !== '/') {
+  dataLayer = {
+    page: {
+      articleTitle: pageDir,
+      name: `fund-managers/news/${pageDir}`,
+    },
+  };
 } else {
-	dataLayer = {
-		"page": {
-			"name": 'fund-managers/home'
-		}
-	}
+  dataLayer = {
+    page: {
+      name: 'fund-managers/home',
+    },
+  };
 }
