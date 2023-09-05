8054e586b38a491bc7d0ced4ef87c3a7a1a763d1
Felix Delval <fdelval@adobe.com>
2023-09-05 14:58:58 +0200

    Putting the Audience Manager in delayed

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,10 +1,15 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+import { loadScript } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+loadScript('//assets.adobedtm.com/launch-ENd6668700e6ad4d64a37c3d34c489ee03.min.js?ver=6.2.2', {
+  async: '',
+});
 
 // Adobe Analytics tracking script
 let dataLayer = {};
